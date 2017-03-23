c1326dc968a286e9667b525276e03681ed45dca2
Kivi Dollin <acd2164@columbia.edu>
2017-03-23 18:30:17 -0400

    codegen should work?

--- codegen.ml
+++ codegen.ml
@@ -15,26 +15,36 @@ http://llvm.moe/ocaml/
 module L = Llvm
 module A = Ast
 
-module StringMap = Map.Make(String)
+exception Error of string
 
-let translate (globals, functions) =
-  let context = L.global_context () in
-  let the_module = L.create_module context "ManiT"
-  and i32_t  = L.i32_type  context
-  and i8_t   = L.i8_type   context
-  and i1_t   = L.i1_type   context
-  and void_t = L.void_type context in
-  
-  (* Declare printf(), which the print built-in function will call *)
-  let print_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-  let print_func = L.declare_function "printf" print_t the_module in
+(* module StringMap = Map.Make(String)
 
-  (* Construct code for an expression; return its value *)
-  let rec expr llbuilder = function
-  A.Call ("print", [e]) ->
-    L.build_call print_func [| (expr llbuilder e) |]
-    "print" llbuilder
-  in
+let translate (globals, functions) = *)
 
-  List.iter build_function_body functions;
-  the_module
+let context = L.global_context ()
+let the_module = L.create_module context "ManiT"
+let builder = builder context
+
+let i32_t  = L.i32_type  context;;
+let i8_t   = L.i8_type   context;;
+let i1_t   = L.i1_type   context;;
+let void_t = L.void_type context;;
+
+(* Declare printf(), which the print built-in function will call *)
+let print_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+let print_func = L.declare_function "printf" print_t the_module in
+
+(* Construct code for an expression; return its value *)
+let rec expr llbuilder = function
+  A.IntLiteral i  -> L.build_global_stringptr "Test" "" llbuilder
+  |   A.BoolLiteral b   -> L.build_global_stringptr "Test" "" llbuilder
+  |   A.FloatLiteral f  -> L.build_global_stringptr "Test" "" llbuilder
+  |   A.StringLiteral s   -> L.build_global_stringptr s "" llbuilder
+  |   A.Call(fname, el)   -> (function "print" -> 
+    let print_t = L.var_arg_function_type i32_t [| pointer_type i8_t |] in
+    let print = L.declare_function "print" print_t the_module in
+
+    let s = expr llbuilder (List.hd el) in
+    let zero = const_int i32_t 0 in
+    let s = build_in_bounds_gep s [| zero |] "" llbuilder in
+    build_call printf [| s |] "" llbuilder
