c59f00ff8c3d4fff50987732b62abb82ac31a591
Kivi Dollin <acd2164@columbia.edu>
2017-03-23 21:08:47 -0400

    not quite there

--- codegen.ml
+++ codegen.ml
@@ -3,7 +3,7 @@ module L = Llvm
 module A = Ast
 
 module StringMap = Map.Make(String)
-let translate (globals, statements) = 
+let translate (globals, functions) = 
 	let context = L.global_context () in
 	let the_module = L.create_module context "ManiT"
 	and i32_t  = L.i32_type  context
@@ -15,7 +15,7 @@ let translate (globals, statements) =
 	let print_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
 	let print_func = L.declare_function "printf" print_t the_module in
 
-	let builder = L.builder_at context in
+	let builder = L.builder_at_end context (L.entry_block statements) in
 
 	(* Format string for print call *)
 	let string_format_str = L.build_global_stringptr "%s\n" "sfmt" builder in
@@ -26,10 +26,10 @@ let translate (globals, statements) =
 	            "print" llbuilder
 	in
 	let rec stmt llbuilder = function
-	    A.Expr(e) -> ignore (expr llbuilder e); stmt llbuilder
+	    A.Expr(e) -> ignore (expr llbuilder e)
 
 	in
 	let buildllvm s = stmt builder s
 	in
-	List.iter buildllvm statements;
+	List.iter buildllvm functions;
 	the_module
