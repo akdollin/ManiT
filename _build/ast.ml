b799e65ba047ba3ad99b63e9f629e878b56e472a
Kivi Dollin <acd2164@columbia.edu>
2017-03-23 20:20:45 -0400

    getting closer

---
+++ _build/ast.ml
@@ -0,0 +1,117 @@
+(* Abstract Syntax Tree and functions for printing it *)
+
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
+          And | Or
+
+type uop = Neg | Not
+
+(* Table Literals *)
+type key_literal =
+    IntKey of int
+  | StringKey of string
+
+and table_literal =
+    EmptyTable
+  | ArrayLiteral of expr list
+  | KeyValueLiteral of (key_literal * expr) list
+
+and expr =
+    Id of string
+  | Binop of expr * op * expr
+  | Call of string * expr list
+  | IntLiteral of int
+  | BoolLiteral of bool
+  | StringLiteral of string
+  | FloatLiteral of float
+  | TableLiteral of table_literal
+  | Unop of uop * expr
+  | Assign of string * expr
+  | TableAccess of string * (expr list)
+  | ThisAccess of expr
+  | TableAssign of string * (expr list) * expr
+  | Noexpr
+
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Func of func_decl
+  | Return of expr
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt
+  | While of expr * stmt
+  | Empty
+
+and func_decl = {
+    fname : string;
+    formals: string list;
+    body : stmt list;
+  }
+
+type program = stmt
+
+(* Pretty-printing functions *)
+(*
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | Leq -> "<="
+  | Greater -> ">"
+  | Geq -> ">="
+  | And -> "&&"
+  | Or -> "||"
+
+let string_of_uop = function
+    Neg -> "-"
+  | Not -> "!"
+
+let rec string_of_expr = function
+    Literal(l) -> string_of_int l
+  (*| BoolLiteral(true) -> "true"
+  | BoolLiteral(false) -> "false"*)
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Noexpr -> ""
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+  | Void -> "void"
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
+
+*)
