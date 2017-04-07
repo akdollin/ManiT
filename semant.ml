7b8cdc06d10948ca796a6dab3c1a3ce56852ac80
Kivi Dollin <acd2164@columbia.edu>
2017-04-07 12:13:03 -0400

    fixed types

--- semant.ml
+++ semant.ml
@@ -175,11 +175,33 @@ let rec find (scope : symbol_table) name =
    
   in
   List.iter check_function functions *)
+(*Closed-open range from a to b, e.g. range 1 5 = [1;2;3;4] *)
+let rec range a b =
+  if a=b-1 then
+    [a]
+  else
+    a::(range (a+1) b)
+    
+let have_duplicates compare lst = 
+  let sorted = List.sort compare lst in
+  match sorted with 
+    [] -> false
+    | hd::tl -> fst (List.fold_left (fun (b,last) next -> (b || last=next),next) (false,hd) tl)
+    
+(*Are all the elements of a list the same? *)
+let all_the_same = function
+  | [] -> true
+  | lst ->
+    (let hd = (List.hd lst) in
+    List.for_all ((=) hd) lst)
+    
+let wrap_id str = "_" ^ str ^ "_"
+let unwrap_id s = String.sub s 1 ( (String.length s) - 2)
 
 let check_func_decls func_dec_list =
   (*Make sure that there are no duplicates*)
   let names = List.map fst func_dec_list in
-  if (Util.have_duplicates String.compare names) then
+  if (have_duplicates String.compare names) then
     raise (Failure "Duplicate function names declared!")
   else
     func_dec_list
