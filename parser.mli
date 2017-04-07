42791889d3e14213b042e59e7e22cf29c320e5fa
Kivi Dollin <acd2164@columbia.edu>
2017-04-07 12:20:37 -0400

    changed ast

---
+++ parser.mli
@@ -0,0 +1,39 @@
+type token =
+  | SEMI
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | COMMA
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | ASSIGN
+  | NOT
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | TRUE
+  | FALSE
+  | AND
+  | OR
+  | RETURN
+  | IF
+  | ELSE
+  | FOR
+  | WHILE
+  | INT
+  | BOOL
+  | VOID
+  | VARIABLE
+  | LITERAL of (int)
+  | STRING_LITERAL of (string)
+  | ID of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
