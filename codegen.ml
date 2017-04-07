a23618724e814db261641ce2f3119e07b87f0a74
Kivi Dollin <acd2164@columbia.edu>
2017-04-07 13:58:04 -0400

    big chnage

--- codegen.ml
+++ codegen.ml
@@ -9,6 +9,7 @@ let translate (functions, pstmts) =
   let the_module = L.create_module context "MicroC"
   and i32_t  = L.i32_type  context
   and i8_t   = L.i8_type   context
+  and float_t = L.double_type context
   and i1_t   = L.i1_type   context
   and pointer_t = L.pointer_type
   and void_t = L.void_type context in
