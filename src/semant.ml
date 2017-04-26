00742ff6e5ccc45d77dfd4792c924885d7b363ac
Kivi Dollin <acd2164@columbia.edu>
2017-04-26 00:27:22 -0400

    change semant

--- src/semant.ml
+++ src/semant.ml
@@ -149,9 +149,9 @@ let rec check_expr (env : environment) = function
           Unop(uop, (e, typ)), typ
       | _ -> raise(Failure("unop error"))
       )
-  | Ast.Struct_make(s) -> 
+(*   | Ast.Struct_make(s) -> 
     let tmp_s = try check_struct s with Not_found -> 
-    raise (Exceptions.InvalidStruct s);
+    raise (Exceptions.InvalidStruct s); *)
   (* Function Call *)
   | Ast.Call(name, actuals) ->
     (* check types to each actuals and get types of formals from fdecl. *)
